a662a4a6696efd992cb0f6e1b25de960f3acf04e
Ankit Negi <132642567+iankitnegi@users.noreply.github.com>
2024-12-03 21:12:34 +0530

    Time: 1541 ms (27.22%), Space: 0B (100.00%) - LeetHub

--- 1068-product-sales-analysis-i/1068-product-sales-analysis-i.sql
+++ 1068-product-sales-analysis-i/1068-product-sales-analysis-i.sql
@@ -2,4 +2,8 @@
 SELECT p.product_name, s.year, s.price
 FROM Sales s
 JOIN Product p
-ON s.product_id = p.product_id
+ON s.product_id = p.product_id;
+
+
+
+# JOIN - means INNER JOIN -> common elements from both the tables
