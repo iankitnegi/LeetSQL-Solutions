388287ff00585582a09b8fea52d0ac9d0dd78579
Ankit Negi <132642567+iankitnegi@users.noreply.github.com>
2024-12-03 20:29:26 +0530

    Time: 1199 ms (73.66%), Space: 0B (100.00%) - LeetHub

--- 1378-replace-employee-id-with-the-unique-identifier/1378-replace-employee-id-with-the-unique-identifier.sql
+++ 1378-replace-employee-id-with-the-unique-identifier/1378-replace-employee-id-with-the-unique-identifier.sql
@@ -2,4 +2,9 @@
 SELECT U.unique_id, E.name
 FROM Employees E
 LEFT JOIN EmployeeUNI U
-ON E.id = U.id
+ON E.id = U.id;
+
+
+
+
+# Left Join -  All the columns from the left tables & only matching col from right tables
