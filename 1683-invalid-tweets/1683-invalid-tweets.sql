0e1c49cf68704fe74ba9566d2bc9af4a7966f15d
Ankit Negi <132642567+iankitnegi@users.noreply.github.com>
2024-12-03 20:24:10 +0530

    Time: 630 ms (60.87%), Space: 0B (100.00%) - LeetHub

--- 1683-invalid-tweets/1683-invalid-tweets.sql
+++ 1683-invalid-tweets/1683-invalid-tweets.sql
@@ -1,4 +1,6 @@
 # Write your MySQL query statement below
 SELECT tweet_id
 FROM Tweets
-WHERE LENGTH(content)>15
+WHERE LENGTH(content)>15;
+
+#LENGTH funn is used to find out the length of the string!
